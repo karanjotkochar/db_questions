e49678f96e53013209b626c42c2df06913c63b06
Karanjot Singh Kochar <karanjot.kochar@mcgill.ca>
2024-07-23 13:16:22 -0400

    re-arrange root folder

--- q_175.sql
+++ q_175.sql
@@ -1,3 +1,7 @@
+-- https://leetcode.com/problems/combine-two-tables/
+
+-- Combine Two Tables
+
 SELECT firstName, lastName, city, state FROM Person
 LEFT JOIN Address
 ON Person.personId = Address.personId
