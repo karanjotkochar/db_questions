547866f39258170d73d52769baae48e47bdfe8d5
karanjotkochar <karanjot.kochar@gmail.com>
2024-08-01 23:57:17 -0400

    start q23 @ sql-50

---
+++ sql-50/q_23.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Number of Unique Subjects Taught by Each Teacher
+
