88994a8fd31e00477f94a4ce222c9c0a65a9a815
karanjotkochar <karanjot.kochar@gmail.com>
2024-08-02 00:00:42 -0400

    add q23 @ sql-50

--- sql-50/q_23.sql
+++ sql-50/q_23.sql
@@ -2,6 +2,6 @@
 
 -- Number of Unique Subjects Taught by Each Teacher
 
-SELECT teacher_id, COUNT(DISTINCT(subject_id))
+SELECT teacher_id, COUNT(DISTINCT(subject_id)) AS cnt
 FROM Teacher
 GROUP BY teacher_id
