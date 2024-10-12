a347fab863c66b3a8b7fbb6fcca03dff9fdc7424
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-12 12:44:19 -0400

    add q_38 @ sql-50

--- sql-50/q_38.sql
+++ sql-50/q_38.sql
@@ -2,3 +2,14 @@
 
 -- Exchange seats
 
+SELECT 
+    CASE
+        WHEN id = (SELECT MAX(id) FROM seat) AND (id % 2) = 1
+            THEN id
+        WHEN (id % 2) = 1
+            THEN id + 1
+        ELSE (id -1)
+    END AS id,
+    student
+FROM Seat
+ORDER BY id
