27b37488a9e16f51d055e8a7075ef3a9a89b2998
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-12 12:04:09 -0400

    add q_31 @ sql-50

--- sql-50/q_31.sql
+++ sql-50/q_31.sql
@@ -1,1 +1,14 @@
+-- https://leetcode.com/problems/primary-department-for-each-employee/description/?envType=study-plan-v2&envId=top-sql-50
+
 -- Primary Department for Each Employee
+
+SELECT employee_id, department_id
+FROM Employee
+WHERE employee_id IN 
+    (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(employee_id) = 1
+    ) 
+    OR primary_flag = 'Y'
