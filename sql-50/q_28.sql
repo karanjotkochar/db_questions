5880b580f9d9d5c04560d8c5ca3243c5dc0ba0cc
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-12 11:31:17 -0400

    add q_28 @ sql-50

--- sql-50/q_28.sql
+++ sql-50/q_28.sql
@@ -1,1 +1,11 @@
+-- https://leetcode.com/problems/biggest-single-number/description/?envType=study-plan-v2&envId=top-sql-50
+
 -- Biggest Single Number
+
+SELECT MAX(num) AS num
+FROM (
+    SELECT num 
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+)
