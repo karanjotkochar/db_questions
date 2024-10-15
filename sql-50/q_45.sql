d7e392528cb9b6d461d23137292c82f5b39304cf
Karanjot Singh Kochar <karanjot.kochar@mcgill.ca>
2024-10-15 14:55:52 -0400

    add q_45

--- sql-50/q_45.sql
+++ sql-50/q_45.sql
@@ -2,3 +2,9 @@
 
 -- Patients With a Condition
 
+SELECT *
+FROM Patients
+WHERE conditions LIKE '% DIAB1%'
+   OR conditions LIKE 'DIAB1%'
+   OR conditions LIKE '% DIAB1'
+   OR conditions = 'DIAB1'
