839d613d9d3897397bb6775dfb83704edb194481
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-27 08:23:19 -0500

    add test/window_function v2

--- test/window_function.sql
+++ test/window_function.sql
@@ -1,7 +1,16 @@
 -- Aggregate Window Function
 SELECT 
-Name, Age, Department, Salary, OVER(PARTITION BY Department) AS AVG_SALARY
+    Name, Age, Department, Salary, OVER(PARTITION BY Department) AS AVG_SALARY
 FROM Employee
 
 
 -- Ranking Window Function
+SELECT
+    ROW_NUMBER() OVER(PARTITION BY Department ORDER BY Salary DESC) AS emp_row_no,
+    Name,
+    Department,
+    Salary,
+    RANK() OVER(PARTITION BY Department ORDER BY Salary DESC) AS emp_rank,
+    DENSE_RANK() OVER(PARTITION BY Department ORDER BY Salary DESC) AS emp_dense_rank
+FROM Employee
+
