14025e499b3d6daae01bb4a313aa5fc65fa2535f
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-27 08:51:39 -0500

    add test/cte v2

--- test/cte.sql
+++ test/cte.sql
@@ -5,3 +5,14 @@ WITH cte_name AS (
 )
 
 SELECT * FROM cte_name
+
+-- Second highest salary
+-- Columns: Name, salary
+
+WITH cte AS (
+    DENSE_RANK() OVER (ORDER BY Salary DESC) AS Rank
+    FROM Employee
+)
+SELECT Name 
+FROM cte
+WHERE Rank = 2
