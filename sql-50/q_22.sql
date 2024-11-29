ba6852110c623b0ffa0d1d0e571316f7f25187e0
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-28 23:09:11 -0500

    draft: q_22 v1

--- sql-50/q_22.sql
+++ sql-50/q_22.sql
@@ -2,3 +2,17 @@
 
 -- Game Play Analysis IV
 
+WITH cte AS (
+    SELECT player_id, 
+        DATEDIFF(
+            LEAD(event_date) OVER(PARTITION BY player_id ORDER BY event_date) AS nextdate,
+            event_date) = 1 AS date_diff,
+        RANK() OVER(PARTITION BY player_id ORDER BY event_date DESC) AS ranking
+    FROM Activity
+)
+
+SELECT 
+    ROUND (
+        COUNT(IF(date_diff = 1,player_id, null))/ COUNT(DISTINCT player_id), 2) AS fraction
+FROM cte
+WHERE ranking = 1
