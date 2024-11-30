c53e58f5175a42e6e373e6d2836db2eae8a8b419
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-30 17:48:17 -0500

    add: q_32@sql

--- sql-50/q_32.sql
+++ sql-50/q_32.sql
@@ -1,3 +1,21 @@
 -- https://leetcode.com/problems/triangle-judgement/description/?envType=study-plan-v2&envId=top-sql-50
 
 -- Triangle Judgement
+
+/*
+    To form a triangle:
+    sum of any 2 sides >(greater than) third side
+*/
+
+WITH cte AS (
+    SELECT x, y, z, IF ((x+y)>z && (y+z)>x && (x+z)>y, "Yes", "No") AS triangle
+    FROM Triangle
+)
+
+SELECT * 
+FROM cte
+
+-- OR
+
+SELECT x, y, z, IF ((x+y)>z && (y+z)>x && (x+z)>y, "Yes", "No") AS triangle
+FROM Triangle
