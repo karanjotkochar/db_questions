5d343b6d87b19a356729747b3239e2abb67c3133
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-30 17:30:11 -0500

    add: q_29@sql

--- sql-50/q_29.sql
+++ sql-50/q_29.sql
@@ -2,3 +2,9 @@
 
 -- Customers Who Bought All Products
 
+SELECT customer_id
+FROM Customer
+INNER JOIN Product
+ON Customer.product_key = Product.product_key
+GROUP BY customer_id
+HAVING COUNT(DISTINCT customer.product_key) = (SELECT COUNT(product_key) FROM Product)
