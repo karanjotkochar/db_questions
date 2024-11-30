b563f625e845a05299906204dfcb997faa6a5444
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-30 18:37:39 -0500

    add: draft q_33@sql v2

--- sql-50/q_33.sql
+++ sql-50/q_33.sql
@@ -2,13 +2,17 @@
 
 -- Consecutive Numbers
 
--- Self Join
+-- Self Join: is simply a conceptual reference to joining the table to itself
+-- To specify the join type, using: 'Inner Join' or simply 'Join'
 
--- SELECT L1.num
---     FROM Logs AS L1
---     SELF JOIN Logs AS L2
---     ON L1.id = L2.id - 1
+SELECT L1.num AS ConsecutiveNums
+    FROM Logs AS L1
+    INNER JOIN Logs AS L2 ON L1.id = L2.id - 1
+    INNER JOIN Logs AS L3 ON L1.id = L3.id - 2
+    WHERE L1.num = L2.num
+        AND L1.num = L3.num
 
+-- Second approach
 -- Window Function
 
 WITH cte AS (
