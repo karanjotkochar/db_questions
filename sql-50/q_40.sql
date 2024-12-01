dd5dfeab41845f249c41732760de5146c82fb511
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-30 20:35:51 -0500

    add: q_40@sql

--- sql-50/q_40.sql
+++ sql-50/q_40.sql
@@ -13,6 +13,12 @@ GROUP BY visited_on between '2019-01-01' AND '2019-01-07'
 ORDER BY visited_on ASC
 
 -- Using window function and cte
+-- cte1: calculates rolling sum of amount for the last 6 days for each visited_on
+-- Window: current day and 5 'preceeding days'
+-- cte2: finds the earliest day from whihc cte1 can be started
+
+-- inner join: ensures that only windows with at least 6 days of data are included
+-- datediff: filters out results that don't yet have a full 6-day window of data, help inner join 
 
 WITH cte1 AS (
 	  SELECT DISTINCT visited_on, 
