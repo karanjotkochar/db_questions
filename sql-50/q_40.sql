6a7eac5aab9a72545a8601ca9b017c85cab290b0
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-30 20:30:32 -0500

    add: draft q_40@sql v1

--- sql-50/q_40.sql
+++ sql-50/q_40.sql
@@ -2,3 +2,29 @@
 
 -- Restaurant Growth
 
+-- Only one record
+
+SELECT 
+    visited_on, 
+    SUM(amount) AS amount , 
+    ROUND(AVG(amount), 2) AS average_amount
+FROM Customer
+GROUP BY visited_on between '2019-01-01' AND '2019-01-07'
+ORDER BY visited_on ASC
+
+-- Using window function and cte
+
+WITH cte1 AS (
+	  SELECT DISTINCT visited_on, 
+			 SUM(amount) OVER (ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) AS amount
+	  FROM Customer
+	),
+    cte2 AS (
+        SELECT MIN(visited_on) AS min_visited_on 
+        FROM cte1
+    )
+
+SELECT visited_on, amount, ROUND(amount/7, 2) AS average_amount
+FROM cte1
+INNER JOIN cte2
+ON DATEDIFF(cte1.visited_on, cte2.min_visited_on) >= 6;
