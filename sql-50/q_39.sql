ef85e55943bc33ebffe327b8c77963bd65d359e2
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-30 19:13:12 -0500

    add: draft q_39@sql

--- sql-50/q_39.sql
+++ sql-50/q_39.sql
@@ -2,3 +2,17 @@
 
 -- Movie Rating
 
+SELECT MovieRating.user_id, COUNT(MovieRating.user_id) AS Count, Users.name
+FROM MovieRating
+JOIN Users
+ON MovieRating.user_id = Users.user_id
+GROUP BY MovieRating.user_id
+ORDER BY Count DESC
+
+SELECT MovieRating.movie_id, AVG(MovieRating.rating) AS Avg, Movies.title
+FROM MovieRating
+JOIN Movies
+ON MovieRating.movie_id = Movies.movie_id
+WHERE MovieRating.created_at >= '2020-02-01' AND MovieRating.created_at < '2020-03-01' 
+GROUP BY MovieRating.movie_id
+ORDER BY Avg DESC
