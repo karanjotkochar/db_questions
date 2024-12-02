09032586203007235b10f303d4fb8746aceace06
Karanjot Singh Kochar <78412374+karanjotkochar@users.noreply.github.com>
2024-12-02 16:51:14 +0000

    add: q_35 v2

--- sql-50/q_35.sql
+++ sql-50/q_35.sql
@@ -17,3 +17,13 @@ SELECT person_name FROM (
 WHERE agg_sum <= 1000
 ORDER BY agg_sum DESC
 LIMIT 1
+
+-- using joins (self join)
+SELECT q1.person_name
+FROM Queue AS q1
+JOIN Queue AS q2
+    ON q1.turn >= q2.turn
+GROUP BY q1.turn, q1.person_name
+HAVING SUM(q2.weight) <= 1000
+ORDER BY SUM(q2.weight) DESC
+LIMIT 1
