ee0ebd71fab99298e304f6cd91354182b181f1d6
Karanjot Singh Kochar <78412374+karanjotkochar@users.noreply.github.com>
2024-12-02 16:19:02 +0000

    update: draft q_35 v1

--- sql-50/q_35.sql
+++ sql-50/q_35.sql
@@ -2,3 +2,17 @@
 
 -- Last Person to Fit in the Bus
 
+-- used subquery because WHERE clause is calculated first than SELECT clause, so cannot use ALIAS
+
+SELECT * FROM (
+    SELECT 
+        person_id, 
+        turn, 
+        person_name, 
+        weight,
+        SUM(weight) OVER(ORDER BY turn) AS agg_sum
+    FROM Queue
+)
+WHERE agg_sum <= 1000
+ORDER BY agg_sum DESC
+LIMIT 1
