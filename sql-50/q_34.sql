55381aa8400bb41e91b548e003c0a8d72523eb87
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-02 08:14:34 -0500

    add: q_34 draft v1

--- sql-50/q_34.sql
+++ sql-50/q_34.sql
@@ -1,3 +1,16 @@
 -- https://leetcode.com/problems/product-price-at-a-given-date/description/?envType=study-plan-v2&envId=top-sql-50
 
 -- Product Price at a Given Date
+
+WITH cte AS (
+    SELECT 
+        DISTINCT product_id,
+        new_price,
+        change_date,
+        DENSE_RANK() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS ranking
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
+
+SELECT * FROM cte
+WHERE ranking = 1
