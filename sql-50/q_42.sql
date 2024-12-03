63088c41c7aaced6fa4889ac86d83e25c2462210
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-03 18:40:16 -0500

    add: q_42, comments

--- sql-50/q_42.sql
+++ sql-50/q_42.sql
@@ -2,6 +2,22 @@
 
 -- Investments in 2016
 
+SELECT * 
+FROM Insurance
+
+-- final: sum of total investments
+
+-- condition 1
+-- combined tiv_2015, more than 1, count >1 
+
+-- condition 2
+-- lat, lon not same
+
+SELECT lat, lon
+FROM Insurance
+GROUP BY lat, lon
+HAVING COUNT(*) = 1
+
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM Insurance
 WHERE tiv_2015 IN (
