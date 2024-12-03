57ebaf0aa5249d7df384aab35b3af73cdea1c102
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-03 18:35:17 -0500

    add: q_42

--- sql-50/q_42.sql
+++ sql-50/q_42.sql
@@ -2,23 +2,17 @@
 
 -- Investments in 2016
 
-SELECT * 
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM Insurance
-
--- final: sum of total investments
-ROUND(SUM(tiv_2016), 2) 
-
--- condition 1
-SELECT tiv_2015
-FROM Insurance
-GROUP BY tiv_2015
-HAVING COUNT(*) > 1
-
--- condition 2
--- lat, lon not same
-
-SELECT lat, lon
-FROM Insurance
-GROUP BY lat, lon
-HAVING COUNT(*) = 1
-
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
