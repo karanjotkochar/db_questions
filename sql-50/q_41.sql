1fa759940c98f00f3db10e54ea1593d64b0d47ee
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-03 08:32:04 -0500

    add: q_41

--- sql-50/q_41.sql
+++ sql-50/q_41.sql
@@ -1,3 +1,14 @@
 -- https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/description/?envType=study-plan-v2&envId=top-sql-50
 
 -- Friend Requests II: Who Has the Most Friends
+
+WITH cte AS (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+)
+SELECT id, COUNT(id) AS num 
+FROM cte 
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
