5e1fc2866c5f82e5f51a84c030e3472fece19560
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-03 07:55:20 -0500

    add: q_36 draft v1

--- sql-50/q_36.sql
+++ sql-50/q_36.sql
@@ -2,4 +2,18 @@
 
 -- Count Salary Categories
 
-    
+WITH cte AS (
+    SELECT 
+    account_id, 
+    income, 
+    CASE 
+        WHEN income < '20000' THEN 'Low Salary'
+        WHEN income BETWEEN '20000' AND '50000' THEN 'Average Salary'
+        WHEN income > '50000' THEN 'High Salary'
+        ELSE '0'
+        END AS category
+    FROM Accounts
+)
+SELECT category, COUNT (category) AS accounts_count
+FROM cte
+GROUP BY category
