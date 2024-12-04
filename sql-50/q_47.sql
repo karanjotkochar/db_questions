c5bfd2a73c698a52a0209c59f5998dbad4fc2551
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-03 19:35:19 -0500

    add: q_47

--- sql-50/q_47.sql
+++ sql-50/q_47.sql
@@ -2,3 +2,22 @@
 
 -- Second Highest Salary
 
+-- using cte: calculates the second rank 
+-- but not catches null value or distinct value conditions
+
+WITH cte AS (
+    SELECT
+        id,
+        salary,
+        RANK() OVER(ORDER BY salary DESC) AS ranking
+    FROM Employee
+)
+SELECT salary AS SecondHighestSalary
+FROM cte
+WHERE ranking = 2
+
+-- using subquery
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee)
