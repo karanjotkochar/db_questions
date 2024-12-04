5edabdb1b260f3c171aa1e44ba4b6b2beadbb6d6
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-03 19:51:55 -0500

    add: q_48

--- sql-50/q_48.sql
+++ sql-50/q_48.sql
@@ -2,7 +2,18 @@
 
 -- Group Sold Products By The Date
 
-SELECT 
-    sell_date, COUNT(product) AS num_sold
+-- check any_value function
+
+SELECT sell_date, COUNT(product) AS num_sold, ANY_VALUE(product) AS products
 FROM Activities
 GROUP BY sell_date
+
+-- use group_concat function
+
+SELECT 
+    sell_date, 
+    COUNT(DISTINCT product) AS num_sold, 
+    GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
