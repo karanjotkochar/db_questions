d83d520113a06dc21321bc228572397144d727b3
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-03 22:51:47 -0500

    add: q_49 v1

--- sql-50/q_49.sql
+++ sql-50/q_49.sql
@@ -2,4 +2,14 @@
 
 -- List the Products Ordered in a Period
 
-    
+WITH cte AS (
+    SELECT p.product_id, p.product_name, p.product_category, SUM(o.unit) AS unit, o.order_date
+    FROM Products AS p 
+    JOIN Orders AS o 
+    ON p.product_id = o.product_id
+    WHERE o.order_date >= '2020-02-01' AND o.order_date < '2020-03-01'
+    GROUP BY p.product_id
+)
+SELECT product_name, unit 
+FROM cte
+WHERE unit >= 100
